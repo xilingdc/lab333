46cfb030b5e9b0e3f49ad3126d6c32ad50a79e37
Mr. Wang <Mr. Wang@172.17.42.41>
2019-09-16 13:06:44 -0400

    12345

---
+++ src/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+public class AddressBook {
+
+	private LinkedList <BuddyInfo> info;
+	
+	public void addBuddy(BuddyInfo namea) {
+		
+		if(namea!=null) {
+		info.add(namea);
+		}
+		
+	}
+	
+	public void removeBuddy(int num) {
+		if(num>=0&&num<info.size()) {
+		info.remove(num);
+		}
+	}
+	public static void main (String[] args) {
+		System.out.println("Address Book");
+		BuddyInfo b = new BuddyInfo(null);
+		b.setName("XIL");
+		AddressBook a = new AddressBook();
+		a.addBuddy(b);
+		a.removeBuddy(0);
+		a.addBuddy(b);
+		
+		
+	}
+}
